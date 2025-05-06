92efc96aec7df2142b1bc4c61e9098af029a6804
Mark Thompson <mthompson@bitovi.com>
2025-05-06 04:50:17 -0700

    30 second delay

--- apps/demo-e2e/src/example-2.spec.ts
+++ apps/demo-e2e/src/example-2.spec.ts
@@ -1,7 +1,16 @@
 import { test, expect } from '@playwright/test';
 
+/**
+ * Promise wrapper for setTimeout
+ */
+const delay = (milliseconds: number): Promise<void> => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds) );
+}
+
 test('has title', async ({ page }) => {
   await page.goto('/');
+
+  await delay(30_000);
 
   // Expect h1 to contain a substring.
   expect(await page.locator('h1').innerText()).toContain('Welcome');
