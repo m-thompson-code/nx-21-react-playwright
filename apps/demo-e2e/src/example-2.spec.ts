bc7d67b0fdd91e4882cc6b9fa58d58c2c81456dc
Mark Thompson <mthompson@bitovi.com>
2025-05-06 10:14:19 -0700

    removed delay

--- apps/demo-e2e/src/example-2.spec.ts
+++ apps/demo-e2e/src/example-2.spec.ts
@@ -3,14 +3,14 @@ import { test, expect } from '@playwright/test';
 /**
  * Promise wrapper for setTimeout
  */
-const delay = (milliseconds: number): Promise<void> => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds) );
-}
+// const delay = (milliseconds: number): Promise<void> => {
+//   return new Promise(resolve => setTimeout(resolve, milliseconds) );
+// }
 
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  await delay(30_000);
+  // await delay(30_000);
 
   // Expect h1 to contain a substring.
   expect(await page.locator('h1').innerText()).toContain('Welcome');
